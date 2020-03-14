f3dc07f4e1fdbad7539f3c6f279d21eeec775f52
Nova <novasampoerna@gmail.com>
2020-03-14 13:29:09 +0700

    msster

---
+++ data/koneksi.php
@@ -0,0 +1,21 @@
+<?php 
+
+
+function database(){
+    $base_url =  "https://" . $_SERVER['SERVER_NAME']."";
+    $data = file_get_contents("$base_url/data/database.json",true);
+    $json = json_decode($data, true);
+    return $json;
+}
+function artikel(){
+    $url =  config()['url_database'];
+    $data = file_get_contents($url,true);
+    $json = json_decode($data, true);
+    return $json;
+}
+function config(){
+    $data = file_get_contents("https://gamengroot.herokuapp.com/data/config.json",true);
+    $json = json_decode($data, true);
+    return $json;
+}
+?>
