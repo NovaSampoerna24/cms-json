ab4e257bb04371659a673f08ed86744873648033
Nova <novasampoerna@gmail.com>
2020-03-17 00:57:50 +0700

    master

---
+++ ngindex.php
@@ -0,0 +1,13 @@
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include ('data/koneksi.php');
+include ('model/Artikel.php');
+
+if($_POST){
+$url = $_POST['url'];
+$ngindex = ngindex($url);
+print_r($ngindex);
+}
