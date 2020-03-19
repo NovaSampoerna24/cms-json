79b0ec5986fd7f8e901927edf108ab019efc33e6
Nova <novasampoerna@gmail.com>
2020-03-19 08:22:23 +0700

    master

--- postingan.php
+++ postingan.php
@@ -9,7 +9,7 @@ if($_POST){
     if($type == "blogger"){
     
         $idblogger = @$_POST['idblog'];
-        $data = getpostblogger($idblogger,100,$url);
+        $data = getpostblogger($idblogger,5,$url);
         foreach ($data as $key => $value) {
             $base_url = $GLOBALS['base_url'];
             $response = curlpost($base_url."/savepostingan.php",$value);
